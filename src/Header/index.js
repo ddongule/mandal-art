dc8dd4c884dd1e7c50491836bf227e28f59c436d
bigsaigon333 <likepeppermint@gmail.com>
2021-05-11 10:49:47 +0900

    fix: add unique key prop to icons in header

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import HeaderWrapper from './index.styles';
 import Input from '../Input';
 
@@ -6,16 +7,16 @@ function Header({ handleUserName }) {
     <HeaderWrapper>
       <div className='emoticons'>
         {[...new Array(4)].map((_, index) => (
-          <>
+          <React.Fragment key={index}>
             <div className='heart'>
-              <img src='https://img.icons8.com/fluent/50/fa314a/pixel-heart.png' />
+              <img src='https://img.icons8.com/fluent/50/fa314a/pixel-heart.png' alt='heart' />
             </div>
             {index !== 3 && (
               <div className='cat'>
-                <img src='https://img.icons8.com/small/16/ffffff/pixel-cat.png' />
+                <img src='https://img.icons8.com/small/16/ffffff/pixel-cat.png' alt='cat' />
               </div>
             )}
-          </>
+          </React.Fragment>
         ))}
       </div>
       <Input handleUserName={handleUserName} placeholder='' /> <span>의 만다라트</span>
